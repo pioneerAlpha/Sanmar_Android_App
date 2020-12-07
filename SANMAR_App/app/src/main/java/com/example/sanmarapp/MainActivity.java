538009564ad1eef19e8aeb0855f194431c83141a
Azmain-Kabir-Swaran <66435034+Azmain-Kabir-Swaran@users.noreply.github.com>
2020-12-07 20:55:41 +0600

    Third Commit

--- SANMAR_App/app/src/main/java/com/example/sanmarapp/MainActivity.java
+++ SANMAR_App/app/src/main/java/com/example/sanmarapp/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Patterns;
